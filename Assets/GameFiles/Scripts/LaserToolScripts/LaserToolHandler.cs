62e6e4a183db11f8b54eac041ad624a3b7e9511d
unitedgamedevelopers <krushilgamedev20@gmail.com>
2021-11-16 19:09:55 +0530

    done

---
+++ Assets/GameFiles/Scripts/LaserToolScripts/LaserToolHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserToolHandler : MonoBehaviour
+{
+    #region Properties
+    [Header("Attributes")]
+    [SerializeField] private float rotSpeed = 0f;
+
+    [Header("Components Reference")]
+    [SerializeField] private GameObject coinDent = null;
+    #endregion
+
+    #region MonoBehviour Functions
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            coinDent.SetActive(false);
+        }
+    }
+    #endregion
+}
